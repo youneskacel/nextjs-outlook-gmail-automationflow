6a23a468ef8b4de5f60e4411c9aef0d6445af608
youneskacel <ynskacel@gmail.com>
2024-07-29 20:24:19 +0100

    documentation code

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
-
+  // get session from client
   const {
     data: { session },
   } = await supabase.auth.getSession();
