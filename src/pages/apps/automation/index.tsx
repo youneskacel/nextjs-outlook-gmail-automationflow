315a277c920799309c2cca300fd023dbbd236ba4
youneskacel <ynskacel@gmail.com>
2024-08-03 18:12:41 +0100

    layout changes

--- src/pages/apps/automation/index.tsx
+++ src/pages/apps/automation/index.tsx
@@ -14,7 +14,7 @@ const Index: FC<Props> = (props) => {
     <Box w="100%" h="100vh" overflowY="auto" overflowX={"clip"} bg={"FBFBFB"}>
       <Stack>
         <Stack bg={"white"} p={"4"} borderRadius="lg">
-          <Flex justifyContent="center" py={"4"}>
+          <Flex justifyContent="center" alignItems="start" py={"4"}>
             <Heading size={"sm"}>Automation</Heading>
           </Flex>
           <Flex direction={'column'}>
